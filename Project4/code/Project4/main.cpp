07bab34fa4f4e556e566520b8588ea01851303c7
Mona Anderssen <31306052+monaanderssen@users.noreply.github.com>
2017-11-02 17:40:12 +0100

    Started

--- Project4/code/Project4/main.cpp
+++ Project4/code/Project4/main.cpp
@@ -1,9 +1,30 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <time.h>
+#include "planet.h"
+#include "solver.h"
+#include <cstdio>
+#include "armadillo"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+
+}
+
+void EnergyAndMagnetization(){
+    double E, M, E_2, M_2;
+
+    for(int i=0;i<N;i++){
+        E = (1/N)*(E(i));
+        M = (1/N)*(M(i));
+        E_2 = (1/N)*(E(i)*E(i));
+        M_2 = (1/N)*(M(i)*M(i));
+    }
 }
